df2b73bbe41b3d55e0bad3fe099e56dfe8473472
unkn0wn1122 <david1.duque@ucp.edu.co>
2023-12-01 18:58:30 -0500

    validations have been fixed and colors applied to the feedback representations

--- vpnConnection.sh
+++ vpnConnection.sh
@@ -1,35 +1,81 @@
 #!/bin/bash
 #autor: Unkn0wn1122
 
-option=$1
+#Colours
+greenColour="\e[0;32m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m"
+blueColour="\e[0;34m"
+yellowColour="\e[0;33m"
+purpleColour="\e[0;35m"
+turquoiseColour="\e[0;36m"
+grayColour="\e[0;37m"
 
-if [[ $option == "-h" || $option == "--help" || $option == "" || $option == "--h" ]];then
-    echo "Usage: ./vpnConnection.sh < -on > or < -off >
+function ctrl_c() {
+	echo -e "\n\n ${redColour} [!] Bye... ${endColour}\n"
+	tput cnorm && exit 1
+}
 
-    -on [*]Start Connection with the vpn 
+# Ctrl + c
+trap ctrl_c INT
 
-    -off [*]Close connection"
+function helpPanel() {
+	echo -e "\n\n${grayColour}Usage:${endColour} ${yellowColour}./vpnConnection.sh < -c >, < -d >, < -h >${endColour}
+
+    ${purpleColour}-c${endColour} ${yellowColour}[*]${endColour} ${grayColour}Start Connection with the vpn${endColour}
+
+    ${purpleColour}-d${endColour} ${yellowColour}[*]${endColour} ${grayColour}Close connection${endColour}
+
+    ${purpleColour}-h${endColour} ${yellowColour}[*]${endColour} ${grayColour}this help panel${endColour}\n\n"
+}
+
+function Connect() {
+	ovpnFile=$(find / -type f -name 'lab_*.ovpn' 2>/dev/null &)
+	psId1=$!
+	wait $psId1
+	echo -e "\n ${yellowColour}[+]${endColour} ${grayColour}Starting connection...."${endColour}
+	sleep 1.5
+	check=$(
+		sudo openvpn --config $ovpnFile --daemon >/dev/null 2>&1
+		echo $?
+	)
+	if [ "$check" -eq 0 ]; then
+		echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Established connection"${endColour}
+	else
+		echo -e ${redColour}"\n [!] Connection could not be established"${endColour}
+	fi
+}
+
+function disConnect() {
+	psId=$(pgrep openvpn 2>/dev/null)
+	check=$(
+		sudo kill $psId 2>/dev/null
+		echo $?
+	)
+	if [ "$check" -eq 0 ]; then
+		echo -e "\n ${yellowColour}[+]${endColour} ${grayColour}Turning off connection...."${endColour}
+		sleep 1.5
+	else
+		echo -e "\n ${redColour}[!] There is not an openvpn process running"${endColour}
+	fi
+}
+
+declare -i parameter_counter=0
+
+while getopts "cdh:" arg; do
+	case $arg in
+	c) let parameter_counter+=1 ;;
+	d) let parameter_counter+=2 ;;
+	h) let parameter_counter+=3 ;;
+	esac
+done
+
+if [ $parameter_counter -eq 1 ]; then
+	Connect
+elif [ $parameter_counter -eq 2 ]; then
+	disConnect
+elif [ $parameter_counter -eq 3 ]; then
+	helpPanel
+else
+	helpPanel
 fi
-
-case $option in
-
-    "-on")#case1.
-    check=$(sudo openvpn --config /home/unknown/Downloads/lab_unkn0wn1122.ovpn --daemon > /dev/null 2>&1)
-    echo "[*]Starting connection..."
-    sleep 1
-    if [ $? == 0 ]; then
-        echo "!Connection Succesfully! :)"
-    else
-        echo "!Connection Error! :("
-    fi 
-    ;;#case1.
-
-    "-off")#case2.
-    process_code=$(pgrep openvpn)
-    sudo kill $process_code
-    echo "[*]Shutting down connection..."
-    sleep 1
-    echo "Bye"
-    ;;#case2.
-
-esac
